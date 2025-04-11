1dc59e13b92b544baccf64621d99cdde1def2fff
Akira-Tai <galvin.akiratai@gmail.com>
2025-04-11 10:11:02 -0500

    Add files via upload

--- midterm/src/main/java/com/example/midterm/StudentsController.java
+++ midterm/src/main/java/com/example/midterm/StudentsController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,27 +18,32 @@ public class StudentsController {
 	@Autowired
 	StudentRepository repository;
 	
+	@CrossOrigin(origins="*")
 	@GetMapping("/api/students/sample")
 	void loadSampleStudents() {
 		SampleStudents sampleStudents = new SampleStudents();
 		repository.saveAll(sampleStudents.getStudents());
 	}
 	
+	@CrossOrigin(origins="*")
 	@GetMapping("/api/students")
 	List<Student> getStudents() {
 		return repository.findAll();
 	}
 	
+	@CrossOrigin(origins="*")
 	@GetMapping("/api/students/{id}")
 	Student getStudent(@PathVariable Long id) {
 		return repository.findById(id).orElseThrow(() -> new Error("Error retrieving student with id " + id));
 	}
 	
+	@CrossOrigin(origins="*")
 	@PostMapping("/api/students")
 	void newStudent(@RequestBody Student newStudent) {
 		repository.save(newStudent);
 	}
 	
+	@CrossOrigin(origins="*")
 	@PutMapping("/api/students/{id}")
 	void updateStudent(@RequestBody Student studentUpdate, @PathVariable Long id) {
 		Student student = repository.findById(id).orElseThrow(() -> new Error("Error updating student with id" + id));
@@ -45,6 +51,7 @@ public class StudentsController {
 		repository.save(student);
 	}
 	
+	@CrossOrigin(origins="*")
 	@DeleteMapping("/api/students/{id}")
 	void deleteStudent(@PathVariable Long id) {
 		repository.deleteById(id);
